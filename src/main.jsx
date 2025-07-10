6d7417712b8b1cd42af82e78f5886f1bdf9a3fda
sukanya1992-t <aboli120takalkar@gmail.com>
2025-07-10 11:40:56 +0530

    add tost cointaner

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,13 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.js'
+import { ToastContainer } from 'react-toastify'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <BrowserRouter>
+    <ToastContainer position="top-center"
+        autoClose={3000} />
       <App />
     </BrowserRouter>
   </Provider >
